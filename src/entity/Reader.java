bf17da9470753b2ad0e10942a3980c70f0c74f6f
janikanika <janikanika@mail.ru>
2021-10-14 14:29:04 +0300

    Library

---
+++ src/entity/Reader.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author user
+ */
+public class Reader {
+    private String Firstname;
+    private String Lastname;
+    private String phone;
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String Firstname) {
+        this.Firstname = Firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String Lastname) {
+        this.Lastname = Lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    
+}
--- src/myclasses/App.java
+++ src/myclasses/App.java
@@ -7,6 +7,10 @@ package myclasses;
 
 import entity.Author;
 import entity.Book;
+import entity.History;
+import entity.Reader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -25,12 +29,44 @@ public class App {
         author.setBirthYear(1828);
         authors[0] = author;
         book.setAuthors(authors);
-        System.out.printf("Создана книга %s, автор: %s %s, год издания: %d ",
+        System.out.printf("Создана книга %s, автор: %s %s, год издания: %d%n",
                 book.getBookName(),
                 book.getAuthors()[0].getFirstName(),
                 book.getAuthors()[0].getLastName(),
                 book.getReleaseYear()
                 );
+        Reader reader = new Reader();
+        reader.setFirstname("Ivan");
+        reader.setLastname("Ivanov");
+        reader.setPhone("555235");
+        System.out.printf("Создан пользователь: %s %s, с телефом %s%n",
+                reader.getFirstname(),
+                reader.getLastname(),
+                reader.getPhone()
+             );
+        History history = new History();
+        history.setBook(book);
+        history.setReader(reader);
+        Calendar c = new GregorianCalendar();
+        history.setGiveBook(c.getTime());
+        System.out.printf("Читатель %s %s взял читать книгу \"%s\"%n,%s"
+                ,history.getReader().getFirstname()
+                ,history.getReader().getLastname()
+                ,history.getBook().getBookName()
+                ,history.getGiveBook()
+        );
+        c = new GregorianCalendar();
+        history.setReturnBook(c.getTime());
+        System.out.printf("%n читатель %s %s вернул книгу \"%s\", %s%n"
+                ,history.getReader().getFirstname()
+                ,history.getReader().getLastname()
+                ,history.getBook().getBookName()
+                ,history.getGiveBook()
+        );
+        
+        
+        
+        
         
         
         
